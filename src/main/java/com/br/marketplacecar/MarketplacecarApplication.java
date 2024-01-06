c09cffd90512115a34dad1a23b8bddec02991738
LucasFonseca <dvlfnsc22@gmail.com>
2024-01-06 11:09:52 -0300

    Fisrt version of Marketplace

---
+++ src/main/java/com/br/marketplacecar/MarketplacecarApplication.java
@@ -0,0 +1,13 @@
+package com.br.marketplacecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplacecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplacecarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/marketplacecar/MarketplacecarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.marketplacecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplacecarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
